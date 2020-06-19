ef2443965373bd1bd0ca3fa57805eb91e5cdc98a
IvanetsAS <ivanetc.aleksandr@gmail.com>
2020-06-20 01:31:52 +0300

    Initial commit

---
+++ Classes/User.py
@@ -0,0 +1,23 @@
+class User:
+    def __init__(
+            self,
+            id,
+            first_name,
+            second_name,
+            last_name,
+            image,
+            department,
+            position,
+            achievements
+
+    ):
+        self.id = id
+        self.first_name = first_name,
+        self.second_name = second_name,
+        self.last_name = last_name,
+        self.image = image,
+        self.department = department,
+        self.position = position,
+        self.achievements = image
+
+
